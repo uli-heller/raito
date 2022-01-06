744a4948ff36129197c48f016772c9160de37a7f
Uli Heller <uli.heller@daemons-point.com>
2022-01-06 10:13:03 +0100

    Externalized config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+        root: "",                              // Website Root URL. Eg: "/raito/"
+        name: "Raito",
+        elements: [ "navbar" ],
+        errorMessage: "Page not found",
+        noHash: ["svg","png","jpg"],            // for urls ending with these extensions, don't use hash urls
+        hashBaseUrl:   "#/",
+        noHashBaseUrl: "",
+}
