dd5b5484f3c25c4d12dd8f43713bd844ccb2d4f7
Uli Heller <uli.heller@daemons-point.com>
2022-01-06 07:53:11 +0100

    create-distributions.sh - WIP

--- create-distributions.sh
+++ create-distributions.sh
@@ -2,6 +2,9 @@
 #
 # create-distributions.sh
 #
+set -e
+D="$(dirname "$0")"
+D="$(cd "${D}" && pwd)"
 BN="$(basename "$0")"
 
 TMPDIR="/tmp/${BN}_$(openssl rand -hex 20)_$$~"
@@ -14,10 +17,32 @@ cleanUp () {
 
 trap cleanUp 0 1 2 3 4 5 6 7 8 9 10 12 13 14 15
 
+BRANCH="$(git branch --show-current)"
 VERSION="$(git describe --tags)"
 
 mkdir -p "${TMPDIR}"
 
+mkdir "${TMPDIR}/raito-${VERSION}"
+
+(
+    cd "${D}"
+    tar cf - $(cat "${D}/distributions/common") $(cat "${D}/distributions/standard")
+)|(
+    cd "${TMPDIR}/raito-${VERSION}"
+    tar xf -
+)
+
+mkdir "${TMPDIR}/raito-dp-${VERSION}"
+
+(
+    cd "${D}"
+    tar cf - $(cat "${D}/distributions/common") $(cat "${D}/distributions/dp")
+)|(
+    cd "${TMPDIR}/raito-dp-${VERSION}"
+    tar xf -
+)
+
+( cd "${TMPDIR}"; find . )
 
 cleanUp
 exit "${RC}"
