6200092b8cca5d0522530b54fda720e940cd3c59
Uli Heller <uli.heller@daemons-point.com>
2022-01-08 00:17:34 +0100

    config.elements -> config.top_elements

--- config-dp.js
+++ config-dp.js
@@ -1,7 +1,7 @@
 const config = {
         root: "",                              // Website Root URL. Eg: "/raito/"
         name: "Raito",
-        elements: [ "top.html", "navbar-dp" ],
+        top_elements: [ "top.html", "navbar-dp" ],
         bottom_elements: [ "footer-dp" ],
         errorMessage: "Page not found",
         noHash: ["svg","png","jpg", "html"],            // for urls ending with these extensions, don't use hash urls
--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
         root: "",                              // Website Root URL. Eg: "/raito/"
         name: "Raito",
-        elements: [ "navbar" ],
+        top_elements: [ "navbar" ],
         bottom_elements: [ "footer" ],
         errorMessage: "Page not found",
         noHash: ["svg","png","jpg","html"],            // for urls ending with these extensions, don't use hash urls
