1c5bb3ff15927d2a901503e2f7c4f44f49d59d62
Uli Heller <uli.heller@daemons-point.com>
2022-01-16 16:44:37 +0100

    Configured for github pages

---
+++ config-gh.js
@@ -0,0 +1,10 @@
+const config = {
+        root: "/raito/",                              // Website Root URL. Eg: "/raito/"
+        name: "Raito",
+        top_elements: [ "navbar" ],
+        bottom_elements: [ "footer" ],
+        errorMessage: "Page not found",
+        noHash: ["svg","png","jpg","html"],    // for urls ending with these extensions, don't use hash urls
+        hashBaseUrl:   "#/",
+        noHashBaseUrl: "",
+}
